8521d76c958b3fed13bdfc5b7023dbc4fa289f2b
dario <darioyzf@gmail.com>
2017-02-01 13:25:24 +0100

    todo

--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
 //
+
+
+
+export function checkSpaceInDir(options: { dir: string, extension?: string, recursive?: boolean, interval?: boolean }) {
+
+
+    if (!options) {
+
+        throw Error("No dir provided")
+
+    } else {
+
+
+        // check partition in which there is that folder
+        // check partition free space
+        // remove files (based on extension if extension is provided), by modified time
+
+
+
+
+    }
+
+
+
+}
